d48cfe71ea83adb5ff2a784a1e7ee4d90b91db88
belindagalstyan <81686104+belindagalstyan@users.noreply.github.com>
2021-04-12 16:12:53 +0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -227,3 +227,28 @@ function toggleBookData() {
     bookData.style.display = "block";
   }
 }
+
+function updateData(e) {
+  e.preventDefault();
+  var updatedBook = {};
+  updatedBook.id = document.getElementById("_id").value;
+  updatedBook.fullName = document.getElementById("fname").value;
+  updatedBook.title = document.getElementById("title").value;
+ 
+      $.ajax({
+      type: 'POST',
+      url: "https://cse-120-2021-api-samantha.herokuapp.com/data/update",
+      data: updatedBook,
+      cache: false,
+      dataType : 'json',
+      success: function (data) {
+        console.log("success");
+      },
+      error: function (xhr) {
+        console.error("Error in post", xhr);
+      },
+      complete: function () {
+        console.log("Complete");  
+      }
+    });
+}
