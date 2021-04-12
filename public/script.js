9515debec2cfe4ed17269d44c919b1a59f6309b7
belindagalstyan <81686104+belindagalstyan@users.noreply.github.com>
2021-04-12 16:11:07 +0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,229 @@
+var loadedData = [];
+
+function loadEditItem() {
+    localStorage = window.localStorage;
+    editItem = JSON.parse(localStorage.getItem("editItem"));
+    console.log(editItem);
+    document.getElementById("_id").value = editItem["_id"];
+    document.getElementById("title").value = editItem["title"];
+    document.getElementById("fullname").value = editItem["fullname"];   
+    document.getElementById("author").value = editItem["author"];
+    document.getElementById("colour").value = editItem["colour"];
+    document.getElementById("covertype").value = editItem["covertype"];
+    document.getElementById("othercovervalue").value = editItem["othercovervalue"];      
+    document.getElementById("nofpages").value = editItem["nofpages"];
+    document.getElementById("price").value = editItem["price"];
+    document.getElementById("currency").value = editItem["currency"];
+    document.getElementById("language").value = editItem["language"];
+    document.getElementById("otherlanguagevalue").value = editItem["otherlanguagevalue"];
+    document.getElementById("orglanguage").value = editItem["orglanguage"];
+    document.getElementById("otherorglanguagevalue").value = editItem["otherorglanguagevalue"];
+    document.getElementById("edition").value = editItem["edition"];
+    document.getElementById("dimensions").value = editItem["dimensions"];
+    document.getElementById("publisher").value = editItem["publisher"];
+    document.getElementById("date").value = editItem["date"];
+    document.getElementById("orgdate").value = editItem["orgdate"];
+    document.getElementById("genre").value = editItem["genre"];
+    document.getElementById("agerestr").value = editItem["agerestr"];
+}
+
+function loadTennisEditItem() {
+    localStorage = window.localStorage;
+    editItem = JSON.parse(localStorage.getItem("editItem"));
+    console.log(editItem);
+    document.getElementById("_id").value = editItem["_id"];
+    document.getElementById("fullname").value = editItem["fullname"];
+    document.getElementById("mail").value = editItem["mail"]; 
+    document.getElementById("date").value = editItem["date"];
+    document.getElementById("time").value = editItem["time"];
+    document.getElementById("frequency").value = editItem["frequency"];
+    document.getElementById("otherfrequencyvalue").value = editItem["otherfrequencyvalue"];      
+    document.getElementById("colour").value = editItem["colour"];
+    document.getElementById("favplayer").value = editItem["favplayer"];
+    document.getElementById("competition").value = editItem["competition"];
+    document.getElementById("yes").value = editItem["yes"];
+}
+
+function editData(id) {
+    var tmp = id.split("edit_");
+    var item_id = tmp[1];
+
+    loadedData.forEach(item => {
+        if ( item._id == item_id) {
+            console.log(item); 
+            localStorage = window.localStorage;
+            localStorage.setItem('editItem', JSON.stringify(item));
+            if (item["project"] == "Tennis") {
+            document.location  = "tennisForm.html"; 
+            } else {
+            document.location  = "form.html"; 
+            }
+        }
+    })
+}
+
+function deleteData(id) {
+
+    var r = confirm("Are you sure you want to delete the item with the following ID? " + id);
+    if (r == false) {
+        return;
+    }
+
+    var tmp = {
+        "id": id
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: "https://cse120-2021-api-belinda.herokuapp.com/data/delete",
+        data: tmp,
+        cache: false,
+        dataType : 'json',
+        success: function (data) {
+            console.log("success");
+            document.getElementById("div" + id).style.display = "none";
+        },
+        error: function (xhr) {
+            console.error("Error in post", xhr);
+        },
+        complete: function () {
+            console.log("Complete");  
+        }
+    });
+}
+
+function saveData() {
+	var tmp = {
+		"test": "Data"
+	}
+
+  $.ajax({
+      type: 'POST',
+      url: "https://cse120-2021-api-belinda.herokuapp.com/data",
+      data: tmp,
+      cache: false,
+      dataType : 'json',
+      success: function (data) {
+        console.log("success");
+      },
+      error: function (xhr) {
+          console.error("Error in post", xhr);
+      },
+      complete: function () {
+          console.log("Complete");  
+      }
+  });
+}
+
+function loadExistingData() {
+  myTennisData = [];
+  myBookData = [];
+  otherData = [];
+  $.ajax({
+      type : "GET",
+      url : "https://cse120-2021-api-belinda.herokuapp.com/data",
+      dataType : "json",
+      success : function(data) {
+        loadedData = data.data;
+        data.data.forEach(elem => {
+          if (elem["owner"] == "Belinda Galstyan") {
+            if (elem["project"] == "Tennis") {
+              myTennisData.push(elem);
+            } else {
+              myBookData.push(elem);
+            }
+          } else {
+            otherData.push(elem);
+          }
+        })
+        displayData(myTennisData, "tennisDataContainer");
+        displayData(myBookData, "bookDataContainer");
+        displayData(otherData, "otherDataContainer");
+      },
+      error : function(data) {
+          console.log("Error")
+      }
+  });
+}
+
+function displayData(data, containerDivName) {
+  document.getElementById(containerDivName).innerHTML = "";
+  data.forEach(elem => {
+    var item = document.createElement("div");
+    item.id = "div" + elem["_id"];
+    item.className = "item";
+    if (Object.keys(elem).length == 1) {
+      var span = document.createElement("span");
+      span.innerHTML = "<i>Empty Element with autogenerated ID: </i>" + elem["_id"];
+      item.appendChild(span);
+    }
+    Object.keys(elem).forEach(key => {
+      if (key != "_id") {
+        var span = document.createElement("span");
+
+        var b = document.createElement("b");
+        b.innerHTML = key + ": ";
+        span.appendChild(b);
+        
+        span.className = "item";
+        if (elem[key]) {
+            span.innerHTML += elem[key];
+        } else {
+            var span1 = document.createElement("span");
+            span1.className = "undefined";
+            span1.innerHTML = "N/A";
+            span.appendChild(span1)
+        }
+        item.appendChild(span);
+
+        var br = document.createElement("br");
+        item.appendChild(br);
+      }
+    })
+    var edit_button = document.createElement("button");
+    edit_button.innerHTML = "Edit";
+    edit_button.id = "edit_" + elem["_id"];
+    edit_button.className = "edit";
+    edit_button.addEventListener("click", function(e){
+        editData(e.target.id);
+    }, false);
+    item.appendChild(edit_button);
+
+    var button = document.createElement("button");
+    button.innerHTML = "Delete";
+    button.id = elem["_id"];
+    button.addEventListener("click", function(e){
+        deleteData(e.target.id);
+    }, false);
+    item.appendChild(button);
+    document.getElementById(containerDivName).appendChild(item);
+  })
+
+}
+
+function toggleOtherData() {
+  var otherData = document.getElementById("otherDataContainer");
+  if (otherData.style.display == "block") {
+    otherData.style.display = "none";
+  } else {
+    otherData.style.display = "block";
+  }
+}
+
+function toggleTennisData() {
+  var tennisData = document.getElementById("tennisDataContainer");
+  if (tennisData.style.display == "block") {
+    tennisData.style.display = "none";
+  } else {
+    tennisData.style.display = "block";
+  }
+}
+
+function toggleBookData() {
+  var bookData = document.getElementById("bookDataContainer");
+  if (bookData.style.display == "block") {
+    bookData.style.display = "none";
+  } else {
+    bookData.style.display = "block";
+  }
+}
